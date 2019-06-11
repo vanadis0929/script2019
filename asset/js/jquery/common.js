c6aa54f5e98f4c3a36ec31cfef7c57405e1b7cea
dj_rdm <vanadis0929@gmail.com>
2019-06-11 22:34:42 +0900

    이름 입력 작업

--- asset/js/jquery/common.js
+++ asset/js/jquery/common.js
@@ -9,9 +9,61 @@ $(function(){
         }, 200);     
     }
 
+    function setGreeting(event){
+        if (event.which === 13 ) {
+            const msg = $(this).val();
+            if (msg.length > 0) {
+                $(this).prev().children('strong').text(msg);
+                saveLocalStorage('name', msg);
+                getGreeting();
+            }
+            else {
+                alert('이름을 입력해주세요.')
+            }
+        }   
+    }
+
+    function removeGreeting(){
+        alert('이름을 초기화 합니다.')
+        delLocalStorge('name');
+        $('#greeting').removeClass('apply');
+        getGreeting();
+    }
+
+    function getGreeting(){
+        const NAME = localStorage.getItem('name');
+        if (NAME != null) {
+            $('#greeting').addClass('apply').find('h1 > strong').text(NAME);
+            $('#greeting_text').hide().val('');
+        } 
+        else {
+            $('#greeting_text').show().val('').focus();
+        }
+        
+    }
+
+    function saveLocalStorage(key, value) {
+        localStorage.setItem(key, value);
+    }
+
+    function delLocalStorge(key, value){
+        if ( key && value == null){
+            localStorage.removeItem(key);
+        }
+        else if (key && value != null) {
+            localStorage.removeItem(key, value);
+        }
+        else if (key == null && value == null ) {
+            localStorage.clear();
+        }
+    }
+
 
     function init() {
         setBackground();
+        getGreeting();
+        $('#greeting_text').on('keydown', setGreeting);
+        $('#greeting > div > button').on('click',removeGreeting);
     }
     init();
 
