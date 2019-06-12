f4fc0351ea0f1ff2ff6962b406fd6546ad9b7082
dj_rdm <vanadis0929@gmail.com>
2019-06-12 18:36:01 +0900

    todo 작업중

--- asset/js/jquery/common.js
+++ asset/js/jquery/common.js
@@ -1,13 +1,16 @@
 $(function () {
+
+    /* 배경 */
     function setBackground() {
         const IMGCOUNT = 7;
         const imgRdm = Math.floor(Math.random() * IMGCOUNT) + 1;
-        $('#bg').html("<img src='/asset/images/" + imgRdm + ".jpg' alt='' />");
-        setTimeout(function () {
+        $('#bg').html(`<img src='/asset/images/${imgRdm}.jpg' alt='' />`);
+        setTimeout(() => {
             $('#bg > img').addClass('complete');
         }, 200);
     }
 
+    /* 인사메시지 */
     function setGreeting(event) {
         if (event.which === 13) {
             const msg = $(this).val();
@@ -23,6 +26,8 @@ $(function () {
             }
         }
     }
+
+
 
     function removeGreeting() {
         alert('이름을 초기화 합니다.');
@@ -49,6 +54,33 @@ $(function () {
         }
     }
 
+
+    /* todo 리스트 */
+    function setTodo(event) {
+        const TODO_ITEM = $(this).val();
+        const TODO_LENGTH = $('#todo > li').length;
+        const TODO_ARRAY = [];
+        const TODO_JSON = {
+            id: `todo_${TODO_LENGTH + 1}`,
+            title: TODO_ITEM
+        };
+        if (event.which === 13) {
+            if (TODO_ITEM != null) {
+                TODO_ARRAY.push(TODO_JSON);
+                console.log(JSON.stringify(TODO_JSON))
+                saveLocalStorage('todo', TODO_JSON);
+
+                $('#todo').append(`<li>${TODO_JSON.title}`);
+                $(this).val('');
+            } else {
+                alert('할 일을 입력해 주세요.')
+            }
+        }
+    }
+
+
+
+    /* 로컬스토리지 */
     function saveLocalStorage(key, value) {
         localStorage.setItem(key, value);
     }
@@ -63,12 +95,15 @@ $(function () {
         }
     }
 
+
     function init() {
         setBackground();
 
         getGreeting();
         $('#greeting_text').on('keydown', setGreeting);
         $('#greeting > div > button').on('click', removeGreeting);
+
+        $('#todo_text').on('keydown', setTodo);
     }
     init();
 });
